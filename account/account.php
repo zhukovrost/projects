49c79da455c355c7a6615f537dd620a4ff41f152
rostislave <rosf.zhukov@gmail.com>
2022-12-07 00:07:47 +0300

    Start of developing account system

--- account/account.php
+++ account/account.php
@@ -1,10 +1,47 @@
+<?php
 
+include "../func.php";
+include '../settings.php';
+
+check_the_login();
+
+$conn = new mysqli(HOSTNAME, HOSTUSER, HOSTPASSWORD, HOSTDB);
+conn_check($conn);
+
+$error_array = array(
+
+);
+
+if (isset($_POST['exit'])){
+  setcookie("login", "");
+  header("Location: regenlog.php");
+} else {
+
+  $select_sql = "SELECT name, surname, thirdname, email, LENGTH(password) FROM users WHERE login='".$_COOKIE['login']."'";
+  if ($data_array = $conn -> query($select_sql)){
+    $login = $_COOKIE['userlogin'];
+    foreach ($data_array as $data){
+      $name = $data['name'];
+      $surname = $data['surname'];
+      $email = $data['email'];
+      if ($data['thirdname'] == '' || $data['thirdname'] == null){
+        $thirdname = "Отсутствует";
+      }else{
+        $thirdname = $data['thirdname'];
+      }
+      $password = "";
+    }
+  }
+
+}
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-	<meta charset="UTF-8">
+  <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Sport - account</title>
@@ -12,18 +49,10 @@
 </head>
 
 <body>
-	<main>
-			<div class="container container_account">
-			<a class="home_account" href="../index.html"><img src="../img/Home.svg" alt=""></a>
-					<p class="account_title">Данные пользователя <span class="account_title_user"><?php echo $userlogin; ?></span>:</p>
-					<div class="data_block">
-							<p>Фамилия: <?php echo $surname; ?></p>
-							<p>Имя: <?php echo $name; ?></p>
-							<p>Отчество: <?php if ($thirdname == ""){ echo "Отсутствует"; }else{ echo $thirdname; } ?></p>
-							<p>Email: <?php echo $email; ?></p>
-							<p>Пароль: <?php echo $password; ?></p>
-					</div>
-			</div>
-		</main>
+  <main>
+    <form method="post">
+      <input type="submit" value="Выйти из аккаунта" name="exit">
+    </form>
+  </main>
 </body>
 </html>
