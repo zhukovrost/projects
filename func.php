dfaa0cfa0c55d2e42a2e8088da822740e9ae3e1f
rostislave <rosf.zhukov@gmail.com>
2022-12-08 23:33:37 +0300

    new func

--- func.php
+++ func.php
@@ -1,18 +1,17 @@
 <?php
-	function render($replaces, $tpl_filename){
-		$tpl = file_get_contents($tpl_filename);
-		$tpl = strtr($tpl, $replaces);
-		return $tpl;
-	}
+function render($replaces, $tpl_filename){
+  $tpl = file_get_contents($tpl_filename);
+  $tpl = strtr($tpl, $replaces);
+  return $tpl;
+}
 
-	function conn_check($conn){
-		if ($conn->connect_error) {
-			die("Connection failed: " . $conn->connect_error);
-			return false;
-		}else{
-			return true;
-		}
-	}
+function conn_check($conn){
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }else{
+    return true;
+  }
+}
 
 function check_the_login($way = ""){
   if (empty($_COOKIE['login'])){
@@ -22,48 +21,38 @@ function check_the_login($way = ""){
   }
 }
 
-function session_set_variable($variable, $default = ""){
-    if (empty($_SESSION[$variable])){
-      if (isset($_POST[$variable])){
-        $_SESSION[$variable] = $_POST[$variable];
-        return true;
-      }else{
-        $_SESSION[$variable] = $default;
-        return false;
-      }
-    }else if ($_SESSION[$variable] == ""){
-      if (isset($_POST[$variable])){
-        $_SESSION[$variable] = $_POST[$variable];
-        return true;
-      }else{
-        $_SESSION[$variable] = $default;
-        return false;
-      }
-    }else{
+function not_empty($cond){
+  if (isset($cond)){
+    if ($cond != "" || $cond != null){
       return true;
+    }else{
+      return false;
     }
+  }else{
+    return false;
+  }
 }
 
-	function success_log($login){
-		setcookie("login", $login);
-		# header('Location: ../index.php');
-    header('Location: account.php');
-	}
+function success_log($login){
+  setcookie("login", $login);
+  # header('Location: ../index.php');
+  header('Location: account.php');
+}
 
-	function log_warning($if, $warning){
-		if (isset($_POST['log_done'])){
-			if ($if){
-				echo "<p class='warning'>".$warning."</p>";
-			}
-		}
-	}
+function log_warning($if, $warning){
+  if (isset($_POST['log_done'])){
+    if ($if){
+      echo "<p class='warning'>".$warning."</p>";
+    }
+  }
+}
 
-	function reg_warning($if, $warning){
-		if (isset($_POST['reg_done'])){
-			if ($if){
-				echo "<p class='warning'>".$warning."</p>";
-			}
-		}
-	}
+function reg_warning($if, $warning){
+  if (isset($_POST['reg_done'])){
+    if ($if){
+      echo "<p class='warning'>".$warning."</p>";
+    }
+  }
+}
 
 ?>
