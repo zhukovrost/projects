15fbb5216c1e42bc99246f7265fdb9eca8dee111
Ivan Barbashin <ivanbarbash06@gmail.com>
2023-01-01 14:06:56 +0300

    add css files and delete header.html

--- account/account.php
+++ account/account.php
@@ -107,6 +107,8 @@ if ($data_array = $conn -> query($select_sql)){
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Sport - account</title>
   <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="../css/reset.css">
+  <link rel="stylesheet" href="../css/format.css">
 </head>
 <?php include "../templates/header.html"; ?>
 <body style="height: 100vh">
