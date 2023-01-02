328696a4cdfaa471e9c669c9383c43c2d1b2705c
Ivan Barbashin <ivanbarbash06@gmail.com>
2023-01-02 15:11:25 +0300

    update main page

---
+++ main.js
@@ -0,0 +1,32 @@
+"use strict"
+
+// =========ЭТО ФОРМЫ=========
+function add_answer (button){
+    let el_ans = button.parentNode.lastElementChild;
+    console.log(el_ans)
+
+    let input_ans = document.createElement("input");
+    input_ans.setAttribute("type","text");
+    input_ans.setAttribute("class", "test");
+    
+    el_ans.appendChild(input_ans, button);
+}
+
+function remove_answer (button){
+    let el_ans = button.parentNode.lastElementChild;
+    let InputsArr = el_ans.querySelectorAll('.test')
+    InputsArr[InputsArr.length - 1].remove();
+}
+    
+
+document.body.addEventListener('click', function(e){
+	let node = e.target;
+    if(node.classList.contains('add')){
+        add_answer(node);
+    }
+    else if(node.classList.contains('remove')){
+        remove_answer(node);
+    }
+    
+   
+}, true)
