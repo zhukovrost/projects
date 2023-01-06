b971ae26af4b4eff09d202a06877289781711bbd
rostislave <rosf.zhukov@gmail.com>
2023-01-06 12:16:04 +0300

    index.php

---
+++ index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;600;700&family=Montserrat+Alternates:ital,wght@0,200;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,800&display=swap" rel="stylesheet">
+</head>
+<body>
+<header>
+  <div class="container">
+    <div class="header_title_block">
+      <p class="header_subtitle">Lorem ipsum</p>
+      <h1 class="header_title">LOREM</h1>
+      <p class="header_subtitle">dolor sit amet</p>
+    </div>
+    <nav>
+      <ul>
+        <li><a href="#">Lorem</a>
+          <ul>
+            <li><a href="#">ipsum</a></li>
+            <li><a href="#">ipsum</a></li>
+            <li><a href="#">ipsum</a></li>
+            <li><a href="#">ipsum</a></li>
+            <li><a href="#">ipsum</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Lorem</a>
+          <ul>
+            <li><a href="#">ipsum</a></li>
+            <li><a href="#">ipsum</a></li>
+            <li><a href="#">ipsum</a></li>
+            <li><a href="#">ipsum</a></li>
+            <li><a href="#">ipsum</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Lorem</a>
+          <ul>
+            <li><a href="#">ipsum</a></li>
+            <li><a href="#">ipsum</a></li>
+            <li><a href="#">ipsum</a></li>
+            <li><a href="#">ipsum</a></li>
+            <li><a href="#">ipsum</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Lorem</a>
+          <ul>
+            <li><a href="#">ipsum</a></li>
+            <li><a href="#">ipsum</a></li>
+            <li><a href="#">ipsum</a></li>
+            <li><a href="#">ipsum</a></li>
+            <li><a href="#">ipsum</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Lorem</a>
+          <ul>
+            <li><a href="#">ipsum</a></li>
+            <li><a href="#">ipsum</a></li>
+            <li><a href="#">ipsum</a></li>
+            <li><a href="#">ipsum</a></li>
+            <li><a href="#">ipsum</a></li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</header>
+
+<main>
+  <div class="info">
+    <p>Lorem ipsum?</p>
+  </div>
+  <div class="card_cover card_cover_first">
+    <div class="card">
+      <div class="circle">
+      </div>
+      <div class="content">
+        <h2>Lorem ipsum</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam ut perspiciatis quibusdam voluptates minima delectus necessitatibus, dolorum incidunt fugit officia corporis, numquam illum, ipsum aut quisquam ipsa dolore libero. Labore!</p>
+      </div>
+      <img class="logo" src="img/Books.png" alt="">
+    </div>
+  </div>
+  <div class="card_cover card_cover_second">
+    <div class="card">
+      <div class="circle">
+      </div>
+      <div class="content">
+        <h2>Lorem ipsum</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam ut perspiciatis quibusdam voluptates minima delectus necessitatibus, dolorum incidunt fugit officia corporis, numquam illum, ipsum aut quisquam ipsa dolore libero. Labore!</p>
+      </div>
+      <img class="logo" src="img/Books.png" alt="">
+    </div>
+  </div>
+  <div class="card_cover card_cover_third">
+    <div class="card">
+      <div class="circle">
+      </div>
+      <div class="content">
+        <h2>Lorem ipsum</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam ut perspiciatis quibusdam voluptates minima delectus necessitatibus, dolorum incidunt fugit officia corporis, numquam illum, ipsum aut quisquam ipsa dolore libero. Labore!</p>
+      </div>
+      <img class="logo" src="img/Books.png" alt="">
+    </div>
+  </div>
+  <div class="card_cover card_cover_fourth">
+    <div class="card">
+      <div class="circle">
+      </div>
+      <div class="content">
+        <h2>Lorem ipsum</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam ut perspiciatis quibusdam voluptates minima delectus necessitatibus, dolorum incidunt fugit officia corporis, numquam illum, ipsum aut quisquam ipsa dolore libero. Labore!</p>
+      </div>
+      <img class="logo" src="img/Books.png" alt="">
+    </div>
+  </div>
+
+  <svg class="layer1" width="374" height="243" viewBox="0 0 374 243" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path class="line1" d="M1.49994 242.5C0.666608 219.667 10.2999 179 55.4999 199C68.3333 211.5 96.8 229 108 199C122 161.5 110 138.5 91.5 120C73 101.5 70 64.5 100.5 65.5C131 66.5 148.5 75 155.5 86C162.5 97 197 117.5 208.5 91.5C220 65.5 208.5 39.5 208.5 32.5C208.5 25.5 198.5 2.49999 228.5 0.999994C258.5 -0.500006 288 6.49999 310.5 19.5C333 32.5 367 41 373 32.5" stroke="black" stroke-width="3"/>
+  </svg>
+  <svg class="layer2" width="496" height="174" viewBox="0 0 496 174" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path class="line2" d="M495.5 173.5C474.5 159 419.3 131.5 366.5 137.5C300.5 145 215 140.5 200.5 89C186 37.5 146.5 13.5 115 4.99999C83.5 -3.50001 20 0.999978 0.5 11.5" stroke="black" stroke-width="3"/>
+  </svg>
+  <svg class="layer3" width="449" height="262" viewBox="0 0 449 262" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path class="line3" d="M447 0.5C451 40.8333 445.4 124 391 134C323 146.5 262 127 238.5 108C215 89 150.5 61.5 112.5 68.5C74.4999 75.5 17.9999 112 11.4999 134C4.99989 156 -13.5001 196.5 20.9999 261" stroke="black" stroke-width="3"/>
+  </svg>
+  <svg class="layer4" width="534" height="271" viewBox="0 0 534 271" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path class="line4" d="M64 1C29.3334 32.5 -28.1 110.3 19.5 169.5C79 243.5 133.5 286.5 231.5 264C329.5 241.5 340.5 204 342 159.5C343.5 115 365.5 19 427.5 39.5C489.5 60 533.5 108 533.5 206" stroke="black" stroke-width="3"/>
+  </svg>
+
+
+</main>
+<?php include "templates/footer.html"; ?>
+<script src="main.js"></script>
+</body>
+</html>
