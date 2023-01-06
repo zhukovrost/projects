0b957b930ea643277421986f3116b044f8afdeac
rostislave <rosf.zhukov@gmail.com>
2023-01-06 12:16:48 +0300

    templates + new function + admin system

---
+++ templates/func.php
@@ -0,0 +1,82 @@
+<?php
+
+#------------------ all -----------------------
+
+	function clear_post($loc){
+		if (isset($_POST)){
+			$_POST = [];
+			return true;
+			header('Location: '.$loc);
+		}else{
+			return false;
+		}
+	}
+
+	function conn_check($conn){
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+			return false;
+		}else{
+			return true;
+		}
+	}
+
+	function render($replaces, $tpl_filename){
+		$tpl = file_get_contents($tpl_filename);
+		$tpl = strtr($tpl, $replaces);
+		return $tpl;
+	}
+
+
+function check_the_login($way = ""){
+  if (empty($_COOKIE['login'])){
+    header('Location: '.$way.'regenlog.php?please_log=1');
+  }else if ($_COOKIE['login'] == ""){
+    header('Location: '.$way.'regenlog.php?please_log=1');
+  }
+}
+
+function check_if_admin($conn, $login){
+  $check_sql = "SELECT status FROM users WHERE login='".$login."'";
+  if ($check = $conn->query($check_sql)){
+    foreach ($check as $user){
+      $status = $user['status'];
+    }
+    if ($staus == 'admin'){
+      return true;
+    }else if ($staus == 'user'){
+      return false;
+    }
+  }else{
+    return false;
+  }
+}
+
+#---------------- reg and log ----------------------
+
+	function success_log($login){
+		$_COOKIE['login'] = $login;
+		header('Location: ../index.php');
+	}
+
+	function log_warning($if, $warning){
+		if (isset($_POST['log_done'])){
+			if ($if){
+				echo "<p class='warning'>".$warning."</p>";
+			}
+		}
+	}
+
+	function reg_warning($if, $warning){
+		if (isset($_POST['reg_done'])){
+			if ($if){
+				echo "<p class='warning'>".$warning."</p>";
+			}
+		}
+	}
+
+#------------------ construct ------------------------
+
+
+
+?>
