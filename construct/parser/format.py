c30a2e0b11a784912f334e7514d06fc9b6251538
Ростислав Жуков <rosf.zhukov@gmail.com>
2023-08-06 14:49:31 +0300

    update

--- construct/parser/format.py
+++ construct/parser/format.py
@@ -41,28 +41,36 @@ f2 = open("themes.json")
 themes = load(f2)
 f2.close()
 
-tests = []
 excluded = {}
-tasks = []
-all_themes = []
+tests = []
+questions = []
 
 for unformatted_item in unformatted_data.items():
+
     filename = unformatted_item[0]
     unformatted_test = unformatted_item[1]
-    formatted_test = []
-    formatted_themes = []
+    formatted_test = {}
+
     print(Fore.MAGENTA)
     print(filename, '\n')
     print(Fore.RESET)
     print("Here is an example:\nQuestion: ", unformatted_test["3"][0], "\nAnswer: ", unformatted_test["3"][1], "\n\nChoose the question type:\n1 - One blank field with different possible answers.\n2 - Several blank fields with one answer variant.\n3 - Do not include this test\n", sep='')
     question_type = int(input("Enter 1 or 2 or 3: "))
+
     if question_type != 3:
+        if question_type == 1:
+            question_type = "definite"
+        elif question_type == 2:
+            question_type = "missing_words"
+
         print_test(unformatted_test)
         ok = int(input("Is this test correct? 0 - no, 1 - yes: "))
+
         while ok != 1:
             incorrect_number = input("Enter the question number, which is incorrect: ")
             print_question(unformatted_test, incorrect_number)
             incorrect_option = input("Enter the option:\n1 - Change question.\n2 - Change answer.\n\nLeave the field blank to go back: ")
+
             while incorrect_option == "1" or incorrect_option == "2":
 
                 if incorrect_option == "1":
@@ -85,28 +93,46 @@ for unformatted_item in unformatted_data.items():
             print_test(unformatted_test)
             ok = int(input("Is this test correct? 0 - no, 1 - yes: "))
 
+        name = input("\nEnter the name: ")
         task = input("\nEnter the task / question: ")
+        score = input("\nEnter the question score (leave blank = 1): ")
+        if score != '':
+            score = int(score)
+        else:
+            score = 1
+
         for i in range(len(themes)):
             print(i + 1, '. ', themes[i], sep='')
 
         theme = input("Choose theme (0 - without theme): ")
         if theme.isnumeric():
-            theme = int(theme) - 1
+            theme = int(theme)
         else:
-            theme = -1
+            theme = 0
 
-        for i in range(1, len(unformatted_test) + 1):
-            question_array = [unformatted_test[str(i)][0], 1, -1, [unformatted_test[str(i)][0].replace(' ', '').upper()], "", None]
-            if question_type == 1:
-                question_array[4] = "definite"
-            elif question_type == 2:
-                question_array[4] = "missing_words"
-            formatted_test.append(question_array)
-            formatted_themes.append(theme)
+        formatted_test['id'] = len(tests) + 1
+        formatted_test['name'] = name
+        formatted_test['task'] = task
+        formatted_test['test'] = []
+        formatted_test['themes'] = [theme]
+
+        for unformatted_question in unformatted_test.values():
+            formatted_question = {}
+            formatted_question['id'] = len(questions) + 1
+            formatted_question['question'] = unformatted_question[0]
+            formatted_question['theme'] = theme
+            formatted_question['type'] = question_type
+            formatted_question['variants'] = []
+            formatted_question['right_answers'] = [unformatted_question[1]]
+            formatted_question['score'] = score
+            formatted_question['image'] = 0
+            questions.append(formatted_question)
+            formatted_test['test'].append(len(questions))
 
         tests.append(formatted_test)
-        tasks.append(task)
-        all_themes.append(formatted_themes)
+
+        print(formatted_test)
+
     else:
         excluded[filename] = unformatted_test
 
@@ -116,14 +142,10 @@ f3 = open("excluded.json", "r+")
 f3.write(dumps(excluded))
 f3.close()
 
-f4 = open("formatted_themes.json", "r+")
-f4.write(dumps(all_themes))
+f4 = open("tests.json", "r+")
+f4.write(dumps(tests))
 f4.close()
 
-f5 = open("formatted_tasks.json", "r+")
-f5.write(dumps(tasks))
+f5 = open("questions.json", "r+")
+f5.write(dumps(questions))
 f5.close()
-
-f6 = open("data.json", "r+")
-f6.write(dumps(tests))
-f6.close()
