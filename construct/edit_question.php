0189f276c2bdcfa7b85c02b374b5d331cec8faeb
Ростислав Жуков <rosf.zhukov@gmail.com>
2023-08-25 17:14:57 +0300

    edit question page (backend)

--- construct/edit_question.php
+++ construct/edit_question.php
@@ -11,11 +11,45 @@ if (empty($_GET['id'])){
 
 $title = "EDIT QUESTION";
 
+# ---------- submit actions ----------------
+
 if (isset($_POST['edit'])){
-    # ------- coming soon ---------
-    $sql = "UPDATE questions SET question=''";
+    $new_question = $_POST["question"];
+    if ($_POST['type'] == "radio" || $_POST['type'] == "checkbox"){
+        $new_variants = json_encode($_POST['variants'], 256);
+        $new_right_answers = explode(" ", $_POST["right_answers"]);
+        for ($i = 0; $i < count($new_right_answers); $i++){
+            $new_right_answers[$i] = (int)$new_right_answers[$i] - 1;
+        }
+        $new_right_answers = json_encode($new_right_answers, 256);
+    } else if ($_POST['type'] == "definite" || $_POST['type'] == "missing_words"){
+        $new_variants = '[]';
+        $new_right_answers = json_encode($_POST['right_answers'], 256);
+    } else {
+        $new_variants = '[]';
+        $new_right_answers = '[]';
+    }
+
+    $sql = "UPDATE questions SET question='$new_question', variants='$new_variants', right_answers='$new_right_answers' WHERE id=$id";
+    $conn->query($sql);
 }
 
+# -------------- add variant -----------------
+if (isset($_POST['add']) || isset($_POST['delete'])){
+    if (isset($_POST['add'])){
+        $new_right_answers = $_POST['right_answers'];
+        $new_right_answers[] = '';
+        $new_right_answers = json_encode($new_right_answers, 256);
+    }else{
+        $new_right_answers = $_POST['right_answers'];
+        array_pop($new_right_answers);
+        $new_right_answers = json_encode($new_right_answers, 256);
+    }
+    $sql = "UPDATE questions SET right_answers='$new_right_answers' WHERE id=$id";
+    $conn->query($sql);
+}
+
+# -------------- getting question data ---------------
 $question_data = get_question_data($conn, $id);
 $type = $question_data['type'];
 $right_answers = json_decode($question_data['right_answers']);
@@ -27,24 +61,36 @@ $score = $question_data['score'];
 <body class="edit_question_body">
 <a class="question_back" href="questions.php">Go back</a>
 <form method="post" action="edit_question.php?id=<?php echo $id; ?>">
-    <input type="text" name="question" value="<?php echo $question_data['question']; ?>">
+    <label for="question">Question (type <?php echo $type; ?>):</label>
+    <input type="text" id="question" name="question" value="<?php echo $question_data['question']; ?>">
     <input type="hidden" name="type" value="<?php echo $type; ?>">
-    <?php if ($type == "radio" || $type == "checkbox"){
+    <?php if ($type == "radio" || $type == "checkbox"){ ?>
+        <label>Variants:</label>
+    <?php
         $variants = json_decode($question_data['variants']);
         for ($i = 0; $i < count($variants); $i++){ ?>
             <input type="text" name="variants[<?php echo $i; ?>]" value="<?php echo $variants[$i]; ?>" placeholder="Variant">
     <?php }
         $right_answers_string = '';
         foreach ($right_answers as $right_answer){
-            $right_answers_string .= (string)$right_answer;
-        } ?>
-        <input type="text" name="right_answers" value="<?php echo $right_answers_string; ?>" placeholder="Answer">
-    <?php } else if ($type == "definite" || $type == "missing_words") {
+            $right_answers_string .= (string)((int)$right_answer + 1).' ';
+        }
+        $right_answers_string = rtrim($right_answers_string);
+        ?>
+        <label for="right_answers">Answer(s):</label>
+        <input type="text" id="right_answers" name="right_answers" value="<?php echo $right_answers_string; ?>" placeholder="Answer">
+    <?php } else if ($type == "definite" || $type == "missing_words") { ?>
+        <label>Answer(s):</label>
+    <?php
         for ($i = 0; $i < count($right_answers); $i++){ ?>
             <input type="text" name="right_answers[<?php echo $i; ?>]" value="<?php echo $right_answers[$i]; ?>" placeholder="Answer">
-        <?php } } ?>
+        <?php } ?>
+    <button type="submit" name="add" value="1">Add variant/word</button>
+        <button type="submit" name="delete" value="1">Delete last variant/word</button>
+    <?php } ?>
 
     <button type="submit" name="edit" value="1">Edit</button>
 </form>
 </body>
 </html>
+<?php $conn->close(); ?>
