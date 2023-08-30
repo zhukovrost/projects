805edd00cb5a7b62e557ee2d82462d5c938ac17b
Ivan Barbashin <ivanbarbash06@gmail.com>
2023-08-30 22:22:56 +0300

    fix and add html to profile page

--- user/profile.php
+++ user/profile.php
@@ -18,6 +18,11 @@ include "../templates/settings.php";
 						<p>Тренировался <span>1</span> день назад</p>
 					</div>
 					<div class="content">
+						<p>Занимаюсь спортом, жму 40. Буду рад знакомству!</p>
+						<div>
+							<img src="../img/edit_gray.svg" alt="">
+							<button>Изменить</button>
+						</div>
 						
 					</div>
 				</section>
@@ -40,8 +45,71 @@ include "../templates/settings.php";
 			</section>
 			<section class="other">
 				<section class="friends">
-					
+						<!-- Title and button to search friends -->
+						<div class="title">
+							<h1>Тренировки друзей</h1>
+							<a href=""><img src="../img/search.svg" alt=""></a>
+						</div>
+						<!-- Friends' workout swiper -->
+						<swiper-container class="content swiper_friends" navigation="true">
+							<!-- slide -->
+							<swiper-slide>
+								<!-- friend item -->
+								<a href="" class="item">
+									<img src="../img/man_avatar.svg" alt="">
+									<p>Иван Иванов</p>
+								</a>
+								<a href="" class="item">
+									<img src="../img/man_avatar.svg" alt="">
+									<p>Иван Иванов</p>
+								</a>
+								<a href="" class="item">
+									<img src="../img/man_avatar.svg" alt="">
+									<p>Иван Иванов</p>
+								</a>
+								<a href="" class="item">
+									<img src="../img/man_avatar.svg" alt="">
+									<p>Иван Иванов</p>
+								</a>
+							</swiper-slide>
+							<swiper-slide>
+								<a href="" class="item">
+									<img src="../img/man_avatar.svg" alt="">
+									<p>Иван Иванов</p>
+								</a>
+								<a href="" class="item">
+									<img src="../img/man_avatar.svg" alt="">
+									<p>Иван Иванов</p>
+								</a>
+								<a href="" class="item">
+									<img src="../img/man_avatar.svg" alt="">
+									<p>Иван Иванов</p>
+								</a>
+								<a href="" class="item">
+									<img src="../img/man_avatar.svg" alt="">
+									<p>Иван Иванов</p>
+								</a>
+							</swiper-slide>
+						</swiper-container>
+					</section>
 				</section>
+			</section>
+			<section class="staff">
+				<div class="coach">
+					<p>Тренер: <span>Штангов К.Г.</span></p>
+					<img src="../img/message.svg" alt="">
+					<img src="../img/profile.svg" alt="">
+					<img src="../img/delete.svg" alt="">
+				</div>
+				<div class="users_count">
+					<a href=""><span>25 подписчиков</span></a>
+					<a href=""><span>15 подписок</span></a>
+				</div>
+			</section>
+			<section class="buttons">
+				<button>Редактировать профиль <img src="../img/edit.svg" alt=""></button>
+				<button>Поиск спортсменов <img src="../img/search.svg" alt=""></button>
+				<button>Выйти <img src="../img/logout.svg" alt=""></button>
 			</section>
 		</div>
 	</main>
