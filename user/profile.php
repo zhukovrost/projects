97834a731ab0bef5062084bd8dc75b7cffdf2c66
Ivan Barbashin <ivanbarbash06@gmail.com>
2023-08-31 14:41:02 +0300

    add design to profile page

--- user/profile.php
+++ user/profile.php
@@ -13,57 +13,135 @@ include "../templates/settings.php";
 			<section class="info">
 				<section class="about">
 					<div class="avatar_block">
-						<img src="" alt="">
+						<img src="../img/man_avatar.svg" alt="">
 						<h1>Иван Иванов</h1>
-						<p>Тренировался <span>1</span> день назад</p>
+						<p><span>1</span> день назад</p>
 					</div>
 					<div class="content">
 						<p>Занимаюсь спортом, жму 40. Буду рад знакомству!</p>
-						<div>
-							<img src="../img/edit_gray.svg" alt="">
-							<button>Изменить</button>
-						</div>
-						
+						<button><img src="../img/edit_gray.svg" alt="">Изменить</button>
 					</div>
 				</section>
 				<section class="news">
-					<button>Новая запись</button>
+					<button class="new_post_btn">Новая запись <img src="../img/add.svg" alt=""></button>
 					<section class="item">
-						<div class="info">
-							<img src="../img/man_avatat.svg" alt="">
+						<div class="post_info">
+							<img src="../img/man_avatar.svg" alt="">
 							<div>
 								<p>Иван иванов</p>
-								<p>12.12.1212</p>
+								<p>12.12.2023</p>
 							</div>
-							<img src="../img/more.svg" alt="">
+							<button class="more" ><img src="../img/more.svg" alt=""></button>
 						</div>
 						<div class="content">
 							<p>Начал(а) программу</p>
-							<button>Подробнее <img src="./img/more_white.svg" alt=""></button>
+							<button>Подробнее <img src="../img/more_white.svg" alt=""></button>
 						</div>
 					</section>
 					<section class="item">
-						<div class="info">
-							<img src="../img/man_avatat.svg" alt="">
+						<div class="post_info">
+							<img src="../img/man_avatar.svg" alt="">
 							<div>
 								<p>Иван иванов</p>
 								<p>12.12.1212</p>
 							</div>
-							<img src="../img/more.svg" alt="">
+							<button class="more" ><img src="../img/more.svg" alt=""></button>
 						</div>
 						<div class="content">
 							<p>Закончил(а) программу</p>
-							<button>Подробнее <img src="./img/more_white.svg" alt=""></button>
+							<button>Подробнее <img src="../img/more_white.svg" alt=""></button>
 						</div>
 					</section>
 					<section class="item">
-						<div class="info">
-							<img src="../img/man_avatat.svg" alt="">
+						<div class="post_info">
+							<img src="../img/man_avatar.svg" alt="">
 							<div>
 								<p>Иван иванов</p>
 								<p>12.12.1212</p>
 							</div>
-							<img src="../img/more.svg" alt="">
+							<button class="more" ><img src="../img/more.svg" alt=""></button>
+						</div>
+						<div class="content">
+							<p>Всем привет!</p>
+						</div>
+					</section>
+					<section class="item">
+						<div class="post_info">
+							<img src="../img/man_avatar.svg" alt="">
+							<div>
+								<p>Иван иванов</p>
+								<p>12.12.2023</p>
+							</div>
+							<button class="more" ><img src="../img/more.svg" alt=""></button>
+						</div>
+						<div class="content">
+							<p>Начал(а) программу</p>
+							<button>Подробнее <img src="../img/more_white.svg" alt=""></button>
+						</div>
+					</section>
+					<section class="item">
+						<div class="post_info">
+							<img src="../img/man_avatar.svg" alt="">
+							<div>
+								<p>Иван иванов</p>
+								<p>12.12.1212</p>
+							</div>
+							<button class="more" ><img src="../img/more.svg" alt=""></button>
+						</div>
+						<div class="content">
+							<p>Закончил(а) программу</p>
+							<button>Подробнее <img src="../img/more_white.svg" alt=""></button>
+						</div>
+					</section>
+					<section class="item">
+						<div class="post_info">
+							<img src="../img/man_avatar.svg" alt="">
+							<div>
+								<p>Иван иванов</p>
+								<p>12.12.1212</p>
+							</div>
+							<button class="more" ><img src="../img/more.svg" alt=""></button>
+						</div>
+						<div class="content">
+							<p>Всем привет!</p>
+						</div>
+					</section>
+					<section class="item">
+						<div class="post_info">
+							<img src="../img/man_avatar.svg" alt="">
+							<div>
+								<p>Иван иванов</p>
+								<p>12.12.2023</p>
+							</div>
+							<button class="more" ><img src="../img/more.svg" alt=""></button>
+						</div>
+						<div class="content">
+							<p>Начал(а) программу</p>
+							<button>Подробнее <img src="../img/more_white.svg" alt=""></button>
+						</div>
+					</section>
+					<section class="item">
+						<div class="post_info">
+							<img src="../img/man_avatar.svg" alt="">
+							<div>
+								<p>Иван иванов</p>
+								<p>12.12.1212</p>
+							</div>
+							<button class="more" ><img src="../img/more.svg" alt=""></button>
+						</div>
+						<div class="content">
+							<p>Закончил(а) программу</p>
+							<button>Подробнее <img src="../img/more_white.svg" alt=""></button>
+						</div>
+					</section>
+					<section class="item">
+						<div class="post_info">
+							<img src="../img/man_avatar.svg" alt="">
+							<div>
+								<p>Иван иванов</p>
+								<p>12.12.1212</p>
+							</div>
+							<button class="more" ><img src="../img/more.svg" alt=""></button>
 						</div>
 						<div class="content">
 							<p>Всем привет!</p>
@@ -73,75 +151,241 @@ include "../templates/settings.php";
 			</section>
 			<section class="other">
 				<section class="friends">
-						<!-- Title and button to search friends -->
-						<div class="title">
-							<h1>Тренировки друзей</h1>
-							<a href=""><img src="../img/search.svg" alt=""></a>
-						</div>
-						<!-- Friends' workout swiper -->
-						<swiper-container class="content swiper_friends" navigation="true">
-							<!-- slide -->
-							<swiper-slide>
-								<!-- friend item -->
-								<a href="" class="item">
-									<img src="../img/man_avatar.svg" alt="">
-									<p>Иван Иванов</p>
-								</a>
-								<a href="" class="item">
-									<img src="../img/man_avatar.svg" alt="">
-									<p>Иван Иванов</p>
-								</a>
-								<a href="" class="item">
-									<img src="../img/man_avatar.svg" alt="">
-									<p>Иван Иванов</p>
-								</a>
-								<a href="" class="item">
-									<img src="../img/man_avatar.svg" alt="">
-									<p>Иван Иванов</p>
-								</a>
-							</swiper-slide>
-							<swiper-slide>
-								<a href="" class="item">
-									<img src="../img/man_avatar.svg" alt="">
-									<p>Иван Иванов</p>
-								</a>
-								<a href="" class="item">
-									<img src="../img/man_avatar.svg" alt="">
-									<p>Иван Иванов</p>
-								</a>
-								<a href="" class="item">
-									<img src="../img/man_avatar.svg" alt="">
-									<p>Иван Иванов</p>
-								</a>
-								<a href="" class="item">
-									<img src="../img/man_avatar.svg" alt="">
-									<p>Иван Иванов</p>
-								</a>
-							</swiper-slide>
-						</swiper-container>
-					</section>
+                    <!-- Title and button to search friends -->
+                    <div class="title">
+                        <h1>Тренировки друзей</h1>
+                        <a href=""><img src="../img/search.svg" alt=""></a>
+                    </div>
+                    <!-- Friends' workout swiper -->
+                    <swiper-container class="content swiper_friends" navigation="true">
+                        <!-- slide -->
+                        <swiper-slide>
+                            <!-- friend item -->
+                            <a href="" class="item">
+                                <img src="../img/man_avatar.svg" alt="">
+                                <p>Иван Иванов</p>
+                            </a>
+                            <a href="" class="item">
+                                <img src="../img/man_avatar.svg" alt="">
+                                <p>Иван Иванов</p>
+                            </a>
+                            <a href="" class="item">
+                                <img src="../img/man_avatar.svg" alt="">
+                                <p>Иван Иванов</p>
+                            </a>
+                            <a href="" class="item">
+                                <img src="../img/man_avatar.svg" alt="">
+                                <p>Иван Иванов</p>
+                            </a>
+                        </swiper-slide>
+						<swiper-slide>
+                            <a href="" class="item">
+                                <img src="../img/man_avatar.svg" alt="">
+                                <p>Иван Иванов</p>
+                            </a>
+                            <a href="" class="item">
+                                <img src="../img/man_avatar.svg" alt="">
+                                <p>Иван Иванов</p>
+                            </a>
+                            <a href="" class="item">
+                                <img src="../img/man_avatar.svg" alt="">
+                                <p>Иван Иванов</p>
+                            </a>
+                            <a href="" class="item">
+                                <img src="../img/man_avatar.svg" alt="">
+                                <p>Иван Иванов</p>
+                            </a>
+                    	</swiper-slide>
+					</swiper-container>
+                </section>
+				<section class="staff">
+					<div class="coach">
+						<p>Тренер: <span>Штангов К.Г.</span></p>
+						<button><img src="../img/message.svg" alt=""></button>
+						<button><img src="../img/profile_black.svg" alt=""></button>
+						<button><img src="../img/delete_black.svg" alt=""></button>
+					</div>
+					<div class="doctor">
+						<p>Врач: <span>нет</span></p>
+						<button><img src="../img/add_black.svg" alt=""></button>
+					</div>
+					<div class="users_count">
+						<a href=""><span>25 подписчиков</span></a>
+						<a href=""><span>15 подписок</span></a>
+					</div>
 				</section>
-			</section>
-			<section class="staff">
-				<div class="coach">
-					<p>Тренер: <span>Штангов К.Г.</span></p>
-					<img src="../img/message.svg" alt="">
-					<img src="../img/profile.svg" alt="">
-					<img src="../img/delete.svg" alt="">
-				</div>
-				<div class="users_count">
-					<a href=""><span>25 подписчиков</span></a>
-					<a href=""><span>15 подписок</span></a>
-				</div>
-			</section>
-			<section class="buttons">
-				<button>Редактировать профиль <img src="../img/edit.svg" alt=""></button>
-				<button>Поиск спортсменов <img src="../img/search.svg" alt=""></button>
-				<button>Выйти <img src="../img/logout.svg" alt=""></button>
+				<section class="last_trainings">
+                    <!-- Title -->
+                    <h1>Последние тренировки</h1>
+                    <!-- Trainings content -->
+                    <div class="content">
+                        <!-- Item -->
+                        <section class="item">
+                            <!-- Left part of last exercise item -->
+                            <div class="left">
+                                <!-- Time of training -->
+                                <div>
+                                    <img src="img/time.svg" alt="">
+                                    <p><span>65</span> мин</p>
+                                </div>
+                                <!-- Exercise count of training -->
+                                <div>
+                                    <img src="img/cards.svg" alt="">
+                                    <p><span>10</span> упражнений</p>
+                                </div>
+                            </div>
+                            <!-- Right part of last exercise item -->
+                            <div class="right">
+                                <!-- Muscle groups count of training -->
+                                <div>
+                                    <img src="img/cards.svg" alt="">
+                                    <p><span>3</span> группы мышц</p>
+                                </div>
+                                <!-- Button 'Подробнее' for more info about exercise -->
+                                <div>
+                                    <button>Подробнее <img src="../img/other.svg" alt=""></button>
+                                </div>
+                            </div>
+                        </section>
+                        <section class="item">
+                            <div class="left">
+                                <div>
+                                    <img src="img/time.svg" alt="">
+                                    <p><span>65</span> мин</p>
+                                </div>
+                                <div>
+                                    <img src="img/cards.svg" alt="">
+                                    <p><span>10</span> упражнений</p>
+                                </div>
+                            </div>
+                            <div class="right">
+                                <div>
+                                    <img src="img/cards.svg" alt="">
+                                    <p><span>3</span> группы мышц</p>
+                                </div>
+                                <div>
+                                    <button>Подробнее <img src="../img/other.svg" alt=""></button>
+                                </div>
+                            </div>
+                        </section>
+                        <section class="item">
+                            <div class="left">
+                                <div>
+                                    <img src="img/time.svg" alt="">
+                                    <p><span>65</span> мин</p>
+                                </div>
+                                <div>
+                                    <img src="img/cards.svg" alt="">
+                                    <p><span>10</span> упражнений</p>
+                                </div>
+                            </div>
+                            <div class="right">
+                                <div>
+                                    <img src="img/cards.svg" alt="">
+                                    <p><span>3</span> группы мышц</p>
+                                </div>
+                                <div>
+                                    <button>Подробнее <img src="../img/other.svg" alt=""></button>
+                                </div>
+                            </div>
+                        </section>
+                        <section class="item">
+                            <div class="left">
+                                <div>
+                                    <img src="img/time.svg" alt="">
+                                    <p><span>65</span> мин</p>
+                                </div>
+                                <div>
+                                    <img src="img/cards.svg" alt="">
+                                    <p><span>10</span> упражнений</p>
+                                </div>
+                            </div>
+                            <div class="right">
+                                <div>
+                                    <img src="img/cards.svg" alt="">
+                                    <p><span>3</span> группы мышц</p>
+                                </div>
+                                <div>
+                                    <button>Подробнее <img src="../img/other.svg" alt=""></button>
+                                </div>
+                            </div>
+                        </section>
+                        <section class="item">
+                            <div class="left">
+                                <div>
+                                    <img src="img/time.svg" alt="">
+                                    <p><span>65</span> мин</p>
+                                </div>
+                                <div>
+                                    <img src="img/cards.svg" alt="">
+                                    <p><span>10</span> упражнений</p>
+                                </div>
+                            </div>
+                            <div class="right">
+                                <div>
+                                    <img src="img/cards.svg" alt="">
+                                    <p><span>3</span> группы мышц</p>
+                                </div>
+                                <div>
+                                    <button>Подробнее <img src="../img/other.svg" alt=""></button>
+                                </div>
+                            </div>
+                        </section>
+                        <section class="item">
+                            <div class="left">
+                                <div>
+                                    <img src="img/time.svg" alt="">
+                                    <p><span>65</span> мин</p>
+                                </div>
+                                <div>
+                                    <img src="img/cards.svg" alt="">
+                                    <p><span>10</span> упражнений</p>
+                                </div>
+                            </div>
+                            <div class="right">
+                                <div>
+                                    <img src="img/cards.svg" alt="">
+                                    <p><span>3</span> группы мышц</p>
+                                </div>
+                                <div>
+                                    <button>Подробнее <img src="../img/other.svg" alt=""></button>
+                                </div>
+                            </div>
+                        </section>
+                        <section class="item">
+                            <div class="left">
+                                <div>
+                                    <img src="img/time.svg" alt="">
+                                    <p><span>65</span> мин</p>
+                                </div>
+                                <div>
+                                    <img src="img/cards.svg" alt="">
+                                    <p><span>10</span> упражнений</p>
+                                </div>
+                            </div>
+                            <div class="right">
+                                <div>
+                                    <img src="img/cards.svg" alt="">
+                                    <p><span>3</span> группы мышц</p>
+                                </div>
+                                <div>
+                                    <button>Подробнее <img src="../img/other.svg" alt=""></button>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </section>
+				<section class="buttons">
+					<button>Редактировать профиль <img src="../img/edit.svg" alt=""></button>
+					<button>Поиск спортсменов <img src="../img/search_white.svg" alt=""></button>
+					<button class="logout">Выйти <img src="../img/logout.svg" alt=""></button>
+				</section>
 			</section>
 		</div>
 	</main>
 
 	<?php include "../templates/footer.html" ?>
+
+	<script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-element-bundle.min.js"></script>
 </body>
 </html>
