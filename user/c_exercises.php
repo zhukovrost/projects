fdd756bb79259bd58114e3c96d4acd920a5cccf3
ivanbarbashin <ivanbarbash06@gmail.com>
2023-09-05 13:26:46 +0300

    add js to popup window

--- user/c_exercises.php
+++ user/c_exercises.php
@@ -358,6 +358,34 @@ if (isset($_GET['my']) && is_numeric($_GET['my'])){
 				popupExerciseWindow.style.cssText = `display: flex`;
 			});
 		}
+		
+const feedbackBtn = document.querySelector('.popup_button');
+const popupWindow = document.querySelector('.popup_feedback');
+
+feedbackBtn.addEventListener('click', function(){
+    popupWindow.classList.add("open");
+});
+
+const closeBtn = document.querySelector('.popup_feedback .close');
+closeBtn.addEventListener('click', function(){
+    popupWindow.classList.remove("open");
+});
+
+window.addEventListener('keydown', (e) => {
+if(e.key == "Escape"){
+    popupWindow.classList.remove("open");
+}
+});
+
+document.querySelector('.popup_feedback form').addEventListener('click', event => {
+    event.isClickWithInModal = true;
+});
+
+popupWindow.addEventListener('click', event =>{
+if(event.isClickWithInModal) return;
+    event.currentTarget.classList.remove('open');
+});
+
 	</script>
 </body>
 </html>
