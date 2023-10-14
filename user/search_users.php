2ebf8178019833b6bdc136ca6abd887fbb51433c
Ivan Barbashin <ivanbarbash06@gmail.com>
2023-10-14 13:10:25 +0300

    add search users page

---
+++ user/search_users.php
@@ -0,0 +1,62 @@
+<?php
+include "../templates/func.php";
+include "../templates/settings.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<?php inc_head(); ?>
+<body>
+    <?php include "../templates/header.html" ?>
+
+	<nav class="users-search-nav">
+		<div class="container">
+			<!-- Buttons to (sub / unsub) users -->
+
+			<a class="button-text users-search-nav__item" href="c_exercises.php?my=1">Подписчики <img src="../img/arrow_white.svg" alt=""></a>
+			<!-- Exercise search -->
+			<div class="users-search-nav__search">
+				<input class="users-search-nav__search-input" type="text" placeholder="Искать">
+				<button class="users-search-nav__search-button"><img src="../img/search_black.svg" alt=""></button>
+			</div>
+		</div>
+	</nav>
+
+	<main class="users-list">
+		<div class="container">
+			<div class="user-card">
+				<img class="user-card__image" src="../img/man_avatar.svg" alt="">
+				<p class="user-card__name">Иван Иванов</p>
+				<button class="button-text user-card__button user-card__button--add"><p>Добавить в друзья</p><img src="../img/add.svg" alt=""></button>
+				<!-- <a class="button-text user-card__button"><p>Программа</p><img src="../img/my_programm.svg" alt=""></a> -->
+			</div>
+			<div class="user-card">
+				<img class="user-card__image" src="../img/man_avatar.svg" alt="">
+				<p class="user-card__name">Иван Иванов</p>
+				<button class="button-text user-card__button user-card__button--add"><p>Добавить в друзья</p><img src="../img/add.svg" alt=""></button>
+			</div>
+			<div class="user-card">
+				<img class="user-card__image" src="../img/man_avatar.svg" alt="">
+				<p class="user-card__name">Иван Иванов</p>
+				<button class="button-text user-card__button user-card__button--add"><p>Добавить в друзья</p><img src="../img/add.svg" alt=""></button>
+			</div>
+			<div class="user-card">
+				<img class="user-card__image" src="../img/man_avatar.svg" alt="">
+				<p class="user-card__name">Иван Иванов</p>
+				<button class="button-text user-card__button user-card__button--add"><p>Добавить в друзья</p><img src="../img/add.svg" alt=""></button>
+			</div>
+			<div class="user-card">
+				<img class="user-card__image" src="../img/man_avatar.svg" alt="">
+				<p class="user-card__name">Иван Иванов</p>
+				<button class="button-text user-card__button user-card__button--add"><p>Добавить в друзья</p><img src="../img/add.svg" alt=""></button>
+			</div>
+			<div class="user-card">
+				<img class="user-card__image" src="../img/man_avatar.svg" alt="">
+				<p class="user-card__name">Иван Иванов</p>
+				<button class="button-text user-card__button user-card__button--add"><p>Добавить в друзья</p><img src="../img/add.svg" alt=""></button>
+			</div>
+		</div>
+	</main>
+
+    <?php include "../templates/footer.html" ?>
+</body>
+</html>
