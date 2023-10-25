efbca6b742094a37788ef41c30fbc8b566506086
Ростислав Жуков <rosf.zhukov@gmail.com>
2023-10-25 21:57:41 +0300

    staff bazaaaaa

--- user/staff.php
+++ user/staff.php
@@ -1,28 +1,34 @@
 <?php
 include "../templates/func.php";
 include "../templates/settings.php";
+if ($user_data->get_status() != "user" && $user_data->get_status() != "admin")
+    header("Location: profile.php");
+
+$user_data->set_staff($conn);
+$has_coach = $user_data->coach != NULL;
+$has_doctor = $user_data->doctor != NULL;
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <?php inc_head(); ?>
 <body>
     <?php include "../templates/header.php" ?>
-
 	<main class="staff-cover">
 		<div class="container">
+            <?php if ($has_coach){ ?>
 			<section class="staff-block">
 				<p class="staff-block__title">Тренер</p>
 				<section class="staff-block__header">
-					<img class="staff-block__avatar" src="../img/man_avatar.svg" alt="">
+					<img class="staff-block__avatar" src="<?php echo $user_data->coach->get_avatar($conn); ?>" alt="">
 					<section class="staff-block__info">
 						<div class="staff-block__name">
-							<h1 class="staff-block__name-text">Иван Иванов</h1>
-							<a class="staff-block__profile-link" href=""><img src="../img/profile_black.svg" alt=""></a>
+							<h1 class="staff-block__name-text"><?php echo $user_data->coach->name." ".$user_data->coach->surname; ?></h1>
+							<a class="staff-block__profile-link" href="profile.php?user=<?php echo $user_data->coach->get_id(); ?>"><img src="../img/profile_black.svg" alt=""></a>
 						</div>
 						<div class="staff-block__buttons">
-							<a href="" class="staff-block__button staff-block__button--img"><img src="../img/vk.svg" alt=""></a>
-							<a href="../img/tg.svg" class="staff-block__button staff-block__button--img"><img src="../img/tg.svg" alt=""></a>
-							<button class="button-text staff-block__button staff-block__button--delite"><p>Удалить</p> <img src="../img/delete.svg" alt=""></button>
+                            <!-- <a href="" class="staff-block__button staff-block__button--img"><img src="../img/vk.svg" alt=""></a>
+							<a href="../img/tg.svg" class="staff-block__button staff-block__button--img"><img src="../img/tg.svg" alt=""></a> -->
+							<a href="delete_coach.php?id=<?php echo $user_data->coach->get_id(); ?>" class="button-text staff-block__button staff-block__button--delite"><p>Удалить</p> <img src="../img/delete.svg" alt=""></a>
 						</div>
 					</section>
 				</section>
@@ -98,19 +104,23 @@ include "../templates/settings.php";
 					
 				</section>
 			</section>
+            <?php } else { ?>
+                <p>У вас нет тренера</p>
+            <?php }
+            if ($has_doctor){ ?>
 			<section class="staff-block">
 				<p class="staff-block__title">Врач</p>
 				<section class="staff-block__header">
-					<img class="staff-block__avatar" src="../img/man_avatar.svg" alt="">
+					<img class="staff-block__avatar" src="<?php echo $user_data->doctor->get_avatar($conn); ?>" alt="">
 					<section class="staff-block__info">
 						<div class="staff-block__name">
-							<h1 class="staff-block__name-text">Иван Иванов</h1>
-							<a class="staff-block__profile-link" href=""><img src="../img/profile_black.svg" alt=""></a>
+							<h1 class="staff-block__name-text"><?php echo $user_data->doctor->name." ".$user_data->doctor->surname; ?></h1>
+							<a class="staff-block__profile-link" href="profile.php?user=<?php echo $user_data->doctor->get_id(); ?>"><img src="../img/profile_black.svg" alt=""></a>
 						</div>
 						<div class="staff-block__buttons">
-							<a href="" class="staff-block__button staff-block__button--img"><img src="../img/vk.svg" alt=""></a>
-							<a href="../img/tg.svg" class="staff-block__button staff-block__button--img"><img src="../img/tg.svg" alt=""></a>
-							<button class="button-text staff-block__button staff-block__button--delite"><p>Удалить</p> <img src="../img/delete.svg" alt=""></button>
+							<!--<a href="" class="staff-block__button staff-block__button--img"><img src="../img/vk.svg" alt=""></a>
+							<a href="../img/tg.svg" class="staff-block__button staff-block__button--img"><img src="../img/tg.svg" alt=""></a>-->
+							<a href="delete_doctor.php?id=<?php echo $user_data->doctor->get_id(); ?>" class="button-text staff-block__button staff-block__button--delite"><p>Удалить</p> <img src="../img/delete.svg" alt=""></a>
 						</div>
 					</section>
 				</section>
@@ -149,6 +159,9 @@ include "../templates/settings.php";
 					</div>
 				</section>
 			</section>
+            <?php } else { ?>
+                <p>У вас нет врача</p>
+            <?php } ?>
 		</div>
 	</main>
 
