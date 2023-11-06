e9a67972e6a5eb8bb8fbd235d920b1fe1dd1edff
Ростислав Жуков <rosf.zhukov@gmail.com>
2023-11-06 11:43:42 +0300

    subscription filter

--- user/search_users.php
+++ user/search_users.php
@@ -2,6 +2,13 @@
 include "../templates/func.php";
 include "../templates/settings.php";
 
+if (isset($_GET["subs"]) && $_GET["subs"] == 1)
+    $subs = 1;
+else
+    $subs = 0;
+
+$user_data->set_subscriptions($conn);
+
 $users_array = array();
 if (isset($_POST['search']) && trim($_POST['search']) != ""){
     $searches = explode(' ', $_POST['search']);
@@ -9,7 +16,9 @@ if (isset($_POST['search']) && trim($_POST['search']) != ""){
         $sql = "SELECT users.name, users.surname, users.id, avatars.file FROM users INNER JOIN avatars ON users.avatar=avatars.id WHERE users.id!=".$user_data->get_id()." AND (users.login LIKE '%$search%' OR users.name LIKE '%$search%' OR users.surname LIKE '%$search%')";
         $result = $conn->query($sql);
         foreach ($result as $item){
-            if (!in_array($item, $users_array))
+            if (!in_array($item, $users_array) && $subs == 0)
+                array_push($users_array, $item);
+            else if (!in_array($item, $users_array) && $subs == 1 && in_array($item["id"], $user_data->subscriptions))
                 array_push($users_array, $item);
         }
     }
@@ -17,7 +26,10 @@ if (isset($_POST['search']) && trim($_POST['search']) != ""){
     $sql = "SELECT users.name, users.surname, users.id, avatars.file FROM users INNER JOIN avatars ON users.avatar=avatars.id WHERE users.id!=".$user_data->get_id();
     $result = $conn->query($sql);
     foreach ($result as $item){
-        array_push($users_array, $item);
+        if (!$subs)
+            array_push($users_array, $item);
+        else if ($subs == 1 && in_array($item["id"], $user_data->subscriptions))
+            array_push($users_array, $item);
     }
 }
 
@@ -32,7 +44,11 @@ $user_data->set_subscriptions($conn);
 	<nav class="users-search-nav">
 		<div class="container">
 			<!-- Buttons to (sub / unsub) users -->
-			<a class="button-text users-search-nav__item" href="c_exercises.php?my=1"><p>Подписки</p><img src="../img/arrow_white.svg" alt=""></a>
+            <?php if ($subs){ ?>
+                <a class="button-text users-search-nav__item" href="search_users.php?subs=0"><p>Все</p><img src="../img/arrow_white.svg" alt=""></a>
+            <?php } else { ?>
+                <a class="button-text users-search-nav__item" href="search_users.php?subs=1"><p>Подписки</p><img src="../img/arrow_white.svg" alt=""></a>
+            <?php } ?>
 			<!-- Exercise search -->
 			<form method="post" class="users-search-nav__search">
 				<input class="users-search-nav__search-input" type="text" placeholder="Искать" name="search">
@@ -47,7 +63,10 @@ $user_data->set_subscriptions($conn);
             if (count($users_array) != 0){
                 foreach ($users_array as $user){
                     if ($user_data->get_auth())
-                        print_user_block($user['name'], $user['surname'], $user['file'], $user['id'], in_array($user['id'], $user_data->subscriptions));
+                        if ($subs)
+                            print_user_block($user['name'], $user['surname'], $user['file'], $user['id'], 1);
+                        else
+                            print_user_block($user['name'], $user['surname'], $user['file'], $user['id'], in_array($user['id'], $user_data->subscriptions));
                     else
                         print_user_block($user['name'], $user['surname'], $user['file'], $user['id'], false);
                 }
